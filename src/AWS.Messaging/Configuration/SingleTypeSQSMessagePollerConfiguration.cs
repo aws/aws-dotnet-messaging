cb021aa04dc8e1384d8aa11ad7b3e11070de8d53
Philippe El Asmar <53088140+philasmar@users.noreply.github.com>
2026-01-22 12:41:52 -0500

    Add single message type pollers with raw message support (#310)

---
+++ src/AWS.Messaging/Configuration/SingleTypeSQSMessagePollerConfiguration.cs
@@ -0,0 +1,40 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.\r
+// SPDX-License-Identifier: Apache-2.0
+
+namespace AWS.Messaging.Configuration;
+
+/// <summary>
+/// Internal configuration for polling a single type of messages from SQS
+/// </summary>
+public class SingleTypeSQSMessagePollerConfiguration : SQSMessagePollerConfiguration
+{
+    /// <summary>
+    /// Construct an instance of <see cref="SingleTypeSQSMessagePollerConfiguration" />
+    /// </summary>
+    /// <param name="queueUrl">The SQS QueueUrl to poll messages from.</param>
+    /// <param name="singleMessageType">The single message type to poll for.</param>
+    public SingleTypeSQSMessagePollerConfiguration(string queueUrl, Type singleMessageType) : base(queueUrl)
+    {
+        SingleMessageType = singleMessageType;
+    }
+
+    /// <summary>
+    /// The poller will only attempt to process messages for the specified message type.
+    /// This is useful for queues that are dedicated to a single message type or when enabling raw JSON ingestion
+    /// without requiring type-based routing.
+    /// </summary>
+    public Type SingleMessageType { get; init; }
+
+    /// <summary>
+    /// Message type identifier to use for the single message type poller.
+    /// If not set, the framework will use the configured subscriber mapping for <see cref="SingleMessageType"/>.
+    /// </summary>
+    internal string? SingleMessageTypeIdentifier { get; init; }
+
+    /// <summary>
+    /// Controls how inbound messages are interpreted when this poller is configured for a single message type.
+    /// When 'Supported' (default), inbound messages are expected to be in the CloudEvents envelope format.
+    /// When 'NotSupported', inbound messages are expected to be a raw payload of the single message type.
+    /// </summary>
+    internal MessageEnvelopeMode MessageEnvelopeMode { get; init; } = MessageEnvelopeMode.Supported;
+}
