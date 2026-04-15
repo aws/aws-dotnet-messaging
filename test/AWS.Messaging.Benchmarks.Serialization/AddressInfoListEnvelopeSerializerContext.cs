ddc5d6cedb53c0ce60e89651a263772a27d92eaf
hykajan <hykajan@hotmail.com>
2026-04-15 10:19:42 +0200

    drop benchmarks

--- test/AWS.Messaging.Benchmarks.Serialization/AddressInfoListEnvelopeSerializerContext.cs
+++
@@ -1,8 +0,0 @@
-using System.Text.Json.Serialization;
-using AWS.Messaging.Benchmarks.Serialization;
-
-[JsonSerializable(typeof(AddressInfoListEnvelope))]
-[JsonSourceGenerationOptions(UseStringEnumConverter = true)]
-public partial class AddressInfoListEnvelopeSerializerContext : JsonSerializerContext
-{
-}
